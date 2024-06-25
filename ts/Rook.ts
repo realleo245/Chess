f67b5a2a44718ce3ef282f34041d34517bd31c6b
realleo245 <leolian2023@outlook.com>
2024-06-25 14:07:58 -0700

    Adjust to not create new Square instance

--- ts/Rook.ts
+++ ts/Rook.ts
@@ -6,16 +6,16 @@ class Rook extends Piece {
     public getAllLegalMoves(): Square[] {
         let moves: Square[] = [];
         for(let i: number = 0; i < this.location.getRow(); i++) {
-            moves.push(new Square(i, this.location.getCol()));
+            moves.push(Game.getInstance().getBoard()[i][this.location.getCol()]);
         }
         for(let i: number = this.location.getRow() + 1; i < 8; i++) {
-            moves.push(new Square(i, this.location.getCol()));
+            moves.push(Game.getInstance().getBoard()[i][this.location.getCol()]);
         }
         for(let i: number = 0; i < this.location.getCol(); i++) {
-            moves.push(new Square(this.location.getRow(), i));
+            moves.push(Game.getInstance().getBoard()[this.location.getRow()][i]);
         }
         for(let i: number = this.location.getCol() + 1; i < this.location.getCol(); i++) {
-            moves.push(new Square(this.location.getRow(), i));
+            moves.push(Game.getInstance().getBoard()[this.location.getRow()][i]);
         }
         return moves;
     }
