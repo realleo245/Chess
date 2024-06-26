56d0dc3f6b674e3fc4847b54110121ea9ca49f58
realleo245 <leolian2023@outlook.com>
2024-06-26 12:03:37 -0700

    Did stuff

--- js/script.ts
+++ js/script.ts
@@ -1,22 +1,22 @@
 // Placeholder
-//window.onload = function() { 
 document.addEventListener("DOMContentLoaded", function() {
     let piece = undefined;
     let cell = undefined;
-    document.getElementById("game").addEventListener("click", (e) => {
-        console.log("click detected");
-        let target = e.target;
-        if(target && target.nodeName == "TD") {
-            if(piece === undefined) {
-                cell = target;
-                piece = cell.textContent;
-            }
-            else {
-                cell.textContent = "";
-                target.textContent = piece;
-                piece = undefined;
-            }
-        }
-        
-    });  
+    document.getElementById("start").addEventListener("click", function() {
+        document.getElementById("game").addEventListener("click", (e) => {
+            console.log("click detected");
+            let target = e.target as Node;
+            if(target && target.nodeName == "TD") {
+                if(piece === undefined) {
+                    cell = target;
+                    piece = cell.textContent;
+                    cell.textContent = "";
+                }
+                else {
+                    target.textContent = piece;
+                    piece = undefined;
+                }
+            }          
+        });  
+    }); 
 });
